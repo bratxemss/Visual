1be617a2817215781c730f44cbd6d9ebe14c3b04
bratxemss <91002197+bratxemss@users.noreply.github.com>
2022-12-12 19:43:48 +0200

    Add files via upload

---
+++ MM/MM/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsFormsApp22
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(72, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(89, 62);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(70, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 62);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(72, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(91, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 52);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 52);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(89, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 52);
+            this.button4.TabIndex = 9;
+            this.button4.Text = ":";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 311);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Decimal calculator";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ MM/MM/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace WindowsFormsApp22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int a;
+            int b;
+            int d;
+            int x;
+            int g;
+            int x1;
+            int d1;
+            int p;
+            int o;
+
+
+            a = Convert.ToInt32(textBox3.Text);
+            b = Convert.ToInt32(textBox4.Text);
+            d = Convert.ToInt32(textBox1.Text);
+            x = Convert.ToInt32(textBox2.Text);
+
+            if ((a >= b) && (a % b == 0))
+            {
+
+                g = a / b;
+                x1 = x * g;
+                p = x1 + d;
+                label1.Text = Convert.ToString(p);
+                label2.Text = Convert.ToString(a);
+
+            }
+            else if ((b >= a) && (b % a == 0))
+            {
+                g = b / a;
+                d1 = d * g;
+                p = d1 + x;
+                label1.Text = Convert.ToString(p);
+                label2.Text = Convert.ToString(b);
+
+            }
+            else
+            {
+                o = a * b;
+                p = (d * b) + (x * a);
+                label1.Text = Convert.ToString(p);
+                label2.Text = Convert.ToString(o);
+
+            }
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            int a;
+            int b;
+            int d;
+            int x;
+            int g;
+            int x1;
+            int d1;
+            int p;
+            int o;
+
+
+            a = Convert.ToInt32(textBox3.Text);
+            b = Convert.ToInt32(textBox4.Text);
+            d = Convert.ToInt32(textBox1.Text);
+            x = Convert.ToInt32(textBox2.Text);
+
+            if ((a >= b) && (a % b == 0))
+            {
+
+                g = a / b;
+                x1 = x * g;
+                p = d - x1;
+                label1.Text = Convert.ToString(p);
+                label2.Text = Convert.ToString(a);
+
+            }
+            else if ((b >= a) && (b % a == 0))
+            {
+                g = b / a;
+                d1 = d * g;
+                p = d1 - x;
+                label1.Text = Convert.ToString(p);
+                label2.Text = Convert.ToString(b);
+
+            }
+            else
+            {
+                o = a * b;
+                p = (d * b) - (x * a);
+                label1.Text = Convert.ToString(p);
+                label2.Text = Convert.ToString(o);
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a;
+            int b;
+            int x;
+            int d;
+            int p;
+            int o;
+            a = Convert.ToInt32(textBox3.Text);
+            b = Convert.ToInt32(textBox4.Text);
+            d = Convert.ToInt32(textBox1.Text);
+            x = Convert.ToInt32(textBox2.Text);
+            p = x * d;
+            o = b * a;
+            label1.Text = Convert.ToString(p);
+            label2.Text = Convert.ToString(o);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a;
+            int b;
+            int x;
+            int d;
+            int p;
+            int o;
+            a = Convert.ToInt32(textBox3.Text);
+            b = Convert.ToInt32(textBox4.Text);
+            d = Convert.ToInt32(textBox1.Text);
+            x = Convert.ToInt32(textBox2.Text);
+            p = b * d;
+            o = x * a;
+            label1.Text = Convert.ToString(p);
+            label2.Text = Convert.ToString(o);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
