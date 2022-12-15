cb4b732cfab590c8b1f97a25cedbb8734715f99b
bratxemss <91002197+bratxemss@users.noreply.github.com>
2022-12-15 17:34:39 +0200

    Add files via upload

---
+++ 8/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<string> girlNames = new List<string>();
+        List<string> boyNames = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            girlNames = System.IO.File.ReadAllLines("GirlNames.txt").ToList();
+            boyNames = System.IO.File.ReadAllLines("BoyNames.txt").ToList();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+            string girlName = textBox1.Text;
+            string boyName = textBox2.Text;
+
+        
+            if (girlNames.Contains(girlName))
+            {
+                MessageBox.Show($"{girlName} is among the most popular names given to girls born in Estonia.");
+            }
+            else
+            {
+                MessageBox.Show($"{girlName} is not among the most popular names given to girls born in Estonia.");
+            }
+
+            if (boyNames.Contains(boyName))
+            {
+                MessageBox.Show($"{boyName} is among the most popular names given to boys born in Estonia.");
+            }
+            else
+            {
+                MessageBox.Show($"{boyName} is not among the most popular names given to boys born in Estonia.");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 9/7/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace _7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+            char mostFrequentCharacter = FindMostFrequentCharacter(input);
+
+            MessageBox.Show("The most frequent character in the string is: " + mostFrequentCharacter);
+        }
+        private char FindMostFrequentCharacter(string input)
+        {
+            // Create a dictionary to keep track of how many times each character appears in the string
+            Dictionary<char, int> charCounts = new Dictionary<char, int>();
+
+            // Loop through each character in the input string
+            foreach (char c in input)
+            {
+                // If the character is not in the dictionary, add it with a count of 1
+                if (!charCounts.ContainsKey(c))
+                {
+                    charCounts.Add(c, 1);
+                }
+                else
+                {
+                    // If the character is already in the dictionary, increment its count by 1
+                    charCounts[c]++;
+                }
+            }
+
+            // Find the character with the highest count
+            char mostFrequentCharacter = '\0'; // null character
+            int maxCount = 0;
+            foreach (KeyValuePair<char, int> kvp in charCounts)
+            {
+                if (kvp.Value > maxCount)
+                {
+                    mostFrequentCharacter = kvp.Key;
+                    maxCount = kvp.Value;
+                }
+            }
+
+            return mostFrequentCharacter;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
